7ee01f2db6202ffc80031de9d148b4966842532c
telecom <zhangql1@chinatelecom.cn>
2020-07-15 16:29:49 +0800

    更新springboot-restful模块，新增对请求路径是通配符**的处理示例代码，。

--- springboot-restful/src/main/java/com/example/restful/controller/PathParamController.java
+++ springboot-restful/src/main/java/com/example/restful/controller/PathParamController.java
@@ -1,10 +1,9 @@
 package com.example.restful.controller;
 
 
+import com.example.restful.annotation.WildcardParam;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
@@ -39,4 +38,58 @@ public class PathParamController {
         log.info("url={}", request.getRequestURL());
         log.info("scheme:{}, mount:{}, dir:{}", scheme, mount, dir);
     }
+
+    /**
+     * 表示name满足4--32位以字母开头的字母与数字字符，并且不包括fonts、oauth、webjars、swagger、images这几个词。
+     * 参考 https://blog.csdn.net/liming19890713/article/details/87366137
+     * @author: QL Zhang
+     * @time: 2020/7/15 15:44
+     */
+    @RequestMapping(value = "/test1/{name:(?!fonts|oauth|webjars|swagger|images)[a-z][0-9a-z-]{3,31}}",method = {RequestMethod.GET})
+    public void test1(@PathVariable String name, HttpServletRequest request){
+        log.info("url={}", request.getRequestURL());
+        log.info("执行test1方法，name={}", name);
+    }
+
+    /**
+     * 示例：/test2/ddffe222a/mytest
+     *
+     * @author: QL Zhang
+     * @time: 2020/7/15 16:03
+     */
+    @RequestMapping(value = "/test2/{name:[a-z][0-9a-z-]{3,31}}/mytest",method = {RequestMethod.GET})
+    public void test2(@PathVariable String name, HttpServletRequest request){
+        log.info("url={}", request.getRequestURL());
+        log.info("执行test2方法，name={}", name);
+    }
+
+    /**
+     * 表示name满足4--32位以字母开头的字母与数字字符，
+     *
+     * url=http://localhost:8080/test3/ddffe222a/mytest/dfsaf
+     * @author: QL Zhang
+     * @time: 2020/7/15 15:45
+     */
+    @RequestMapping(value = "/test3/{name:[a-z][0-9a-z-]{3,31}}/**",method = {RequestMethod.GET})
+    public void test3(@PathVariable String name, HttpServletRequest request){
+        log.info("url={}", request.getRequestURL());
+        log.info("执行test3方法，name={}", name);
+    }
+
+    /**
+     * 用**通配符匹配包含/的多种路径，如下示例：
+     * url=http://localhost:8080/test4/zql/ddddd/dfsaf/mytest
+     * **=ddddd/dfsaf/mytest
+     * 自定义注解然后覆写addArgumentResolvers方法将其注入，实现自己对**内容的解析并赋值给变量
+     *
+     * @author: QL Zhang
+     * @time: 2020/7/15 16:25
+     */
+    @GetMapping(value = "/test4/{name}/**")
+    public void test4(@PathVariable String name, @WildcardParam String path, HttpServletRequest request){
+        log.info("url={}", request.getRequestURL());
+        log.info("执行test3方法，name={}", name);
+        log.info("**={}", path);
+    }
+
 }
